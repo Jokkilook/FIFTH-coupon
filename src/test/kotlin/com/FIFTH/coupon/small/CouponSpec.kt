dc401c316672eb49887e684cc0e793fb4642142e
Jokkilook <tjddbs8295@gmail.com>
2024-05-08 16:40:38 +0900

    테스트 임플리멘테이션 추가

--- src/test/kotlin/com/FIFTH/coupon/small/CouponSpec.kt
+++ src/test/kotlin/com/FIFTH/coupon/small/CouponSpec.kt
@@ -1,9 +1,27 @@
 package com.FIFTH.coupon.small
 
+
+import com.FIFTH.coupon.dto.Coupon
+import com.FIFTH.coupon.dto.User
+import com.FIFTH.coupon.repository.CouponRepository
+import com.FIFTH.coupon.repository.UserRepository
+import com.FIFTH.coupon.service.CouponService
+import com.FIFTH.coupon.service.UserService
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.redis.core.StringRedisTemplate
 
 @SpringBootTest
-class CouponSpec {
+class CouponSpec(
+    @Autowired val userRepository:UserRepository,
+    @Autowired val userService: UserService,
+    @Autowired val couponRepository: CouponRepository,
+    @Autowired val couponService: CouponService,
+    @Autowired val redisTemplate: StringRedisTemplate,
+) {
     //여기에 테스트 코드 입력
+
 
 }
