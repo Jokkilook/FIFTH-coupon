34e18d590127e978f3ca0bf58f4d1a1bbd96adb7
Jokkilook <tjddbs8295@gmail.com>
2024-05-08 15:47:21 +0900

    Update CouponSpec.kt

--- src/test/kotlin/com/FIFTH/coupon/small/CouponSpec.kt
+++ src/test/kotlin/com/FIFTH/coupon/small/CouponSpec.kt
@@ -5,5 +5,9 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class CouponSpec {
     //여기에 테스트 코드 입력
-
+    
+    //쿠폰 발급 대상 유저 유효성 테스트
+    fun checkTargetUserValidation(){
+        
+    }
 }
