b5831cf2caa5bb9f17f66fc4ff743270d3d4c103
melonNamedLove <151634498+melonNamedLove@users.noreply.github.com>
2024-05-13 20:37:50 +0900

    small test - 쿠폰데이터 정상 저장여부
    save에서 에러남!

--- src/test/kotlin/com/FIFTH/coupon/small/CouponSpec.kt
+++ src/test/kotlin/com/FIFTH/coupon/small/CouponSpec.kt
@@ -5,6 +5,7 @@ import com.FIFTH.coupon.repository.CouponRepository
 import com.FIFTH.coupon.repository.UserRepository
 import com.FIFTH.coupon.service.CouponService
 import com.FIFTH.coupon.service.UserService
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -12,8 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.data.redis.core.StringRedisTemplate
 import org.mockito.Mockito
 import java.util.concurrent.TimeUnit
-import org.junit.jupiter.api.Assertions.assertFalse
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.mockito.Mock
 import org.springframework.boot.test.mock.mockito.MockBean
 
 @SpringBootTest
@@ -27,6 +27,8 @@ class CouponSpec(
     //여기에 테스트 코드 입력
     @MockBean
     private val userRepo = userRepository
+
+    @MockBean
     private val couponRepo = couponRepository
     
     //쿠폰 발급 대상 유저 유효성 테스트
@@ -104,4 +106,33 @@ class CouponSpec(
             .opsForValue().set(Mockito.eq("$userId$couponId"), Mockito.anyString(), Mockito.eq(minuteTime), Mockito.eq(TimeUnit.MINUTES))
     }
 
+    @Test
+    fun `쿠폰데이터정상저장여부`(){
+        //여기에 API 에서 실행 될 함수 로직 구현
+        val list = arrayOf('1','2','3','4','5','6','7','8','9','0','A','B','C','D','E','F','G','H','I','J','K','L','N','M','O','P','Q','R','S','T','U','V','W','Z','Y','Z')
+
+        //쿠폰코드 생성
+        fun createCouponCode():String{
+            var code:String = ""
+            for( i in 1..8){
+                code += list.random()
+            }
+            return code
+        }
+        var username ="meow"
+        var coupontest=createCouponCode()
+//        assertNull(couponRepo)
+        //유저아이디에 쿠폰코드 매칭해서 쿠폰데이터 생성
+        val meow = Coupon(userId = username, couponCode = coupontest)
+
+        fun createUserCoupon(userId:String):Coupon{
+            val coupon = Coupon(id = 1, userId=userId, couponCode = createCouponCode())
+
+            return  couponRepo.save(coupon)
+        }
+
+//        assertEquals(meow,createUserCoupon("meow") )
+        createUserCoupon(username)
+        assertNotNull(couponRepo.findCouponsByUserId(username))
+    }
 }
