5f343c15049726227048471b8a31bf3d8e034a99
Jokkilook <tjddbs8295@gmail.com>
2024-05-13 11:52:34 +0900

    미디움 라지 테스트 세팅

--- src/test/kotlin/com/FIFTH/coupon/large/CouponControllerSpec.kt
+++ src/test/kotlin/com/FIFTH/coupon/large/CouponControllerSpec.kt
@@ -1,4 +1,5 @@
 package com.FIFTH.coupon.large
+import com.FIFTH.coupon.controller.Controller
 import com.FIFTH.coupon.dto.Coupon
 import com.FIFTH.coupon.dto.User
 import com.FIFTH.coupon.repository.CouponRepository
@@ -14,10 +15,19 @@ import org.mockito.Mockito
 import java.util.concurrent.TimeUnit
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.DisplayName
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
-@SpringBootTest
-class CouponControllerSpec(
+@WebMvcTest(Controller::class)
+class CouponControllerSpec@Autowired constructor(
+    private val mockMvc: MockMvc,
     @Autowired val userRepository:UserRepository,
     @Autowired val userService: UserService,
     @Autowired val couponRepository: CouponRepository,
@@ -25,5 +35,11 @@ class CouponControllerSpec(
     @Autowired val redisTemplate: StringRedisTemplate,
 ) {
     //여기에 테스트 코드 입력
-    
+
+    @MockBean
+    private val userRepo = userRepository
+    private val couponRepo = couponRepository
+
+
+
 }
